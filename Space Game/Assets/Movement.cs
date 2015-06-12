64918b54340a85649083c6fd439212d1c1beacdd
lucasthebear <lucas.stettner@salkschool.org>
2015-06-12 16:41:20 -0400

    init

---
+++ Space Game/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float  horizontalSpeed = 2.0F;
+	public float  verticalSpeed = 2.0F;
+	float speedCurrent =0; 
+	float maxSpeed = 25;
+	float acceleration = 15;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = horizontalSpeed * Input.GetAxis("Horizontal");
+		float v  = verticalSpeed * Input.GetAxis("Vertical");
+		transform.Rotate(v, h, 0);
+
+		bool pressedSpace = Input.GetKey(KeyCode.Space);
+		if(pressedSpace){
+			speedCurrent = speedCurrent + acceleration * Time.deltaTime;
+
+		}
+		else{
+			speedCurrent = speedCurrent - acceleration * Time.deltaTime;
+
+		}
+		speedCurrent = Mathf.Clamp(speedCurrent, 0, maxSpeed);
+		transform.position = transform.position + transform.forward * speedCurrent * Time.deltaTime;
+	}
+}
---
+++ Space Game/Assets/SimpleAI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleAI : MonoBehaviour {
+public GameObject Starship;
+	public float Speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 StarshipPosition = Starship.transform.position;
+		transform.LookAt(StarshipPosition);
+		rigidbody.AddForce(transform.forward*Speed*Time.deltaTime);
+	}
+}
